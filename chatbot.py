13ad132d564afe4b6a79ffb6efb368a32902ad51
한유정 <dbwjdcheld@naver.com>
2025-02-22 04:30:04 +0000

    Drop code

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,12 @@ load_dotenv()
 from langchain.chat_models import init_chat_model
 model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage
 
-response = model.invoke([HumanMessage(content="Hi! I'm Bob")])
-print(response)
+response = model.invoke([
+        HumanMessage(content="Hi! I'm Bob"),
+        AIMessage(content="Hello Bob! How can I assist you today?"),
+        HumanMessage(content="What's my name?"),
+    ])
+
+print(response.content)
